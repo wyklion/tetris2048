3405dbe9e22b287225557bba1b532b96b4aa8a1d
KK <wyklion@qq.com>
2018-12-21 12:29:03 +0800

    Add files via upload

--- frameworks/WeChatGame/game.js
+++ frameworks/WeChatGame/game.js
@@ -11,18 +11,32 @@ require('game.min');
 // 广告条
 var { windowWidth, windowHeight, pixelRatio } = wx.getSystemInfoSync();
 var height = windowHeight * 0.102;
-let bannerAd = wx.createBannerAd({
-   adUnitId: 'adunit-c0ee23e59d3ce0ae',
-   style: {
-      left: 0, top: 0,
-      width: 300,
-   }
-})
-bannerAd.show();
-bannerAd.onResize(res => {
-   bannerAd.style.left = (windowWidth - bannerAd.style.realWidth) * 0.5;
-   bannerAd.style.top = windowHeight - bannerAd.style.realHeight;
-})
+var lastBanner;
+var createBanner = function () {
+   var bannerAd = wx.createBannerAd({
+      adUnitId: 'adunit-c0ee23e59d3ce0ae',
+      style: {
+         left: 0, top: 0,
+         width: 300,
+      }
+   })
+   bannerAd.show();
+   bannerAd.onResize(res => {
+      bannerAd.style.left = (windowWidth - bannerAd.style.realWidth) * 0.5;
+      bannerAd.style.top = windowHeight - bannerAd.style.realHeight;
+   })
+   bannerAd.onLoad(() => {
+      if (lastBanner) {
+         lastBanner.destroy();
+      }
+      lastBanner = bannerAd;
+   });
+   bannerAd.show();
+}
+createBanner();
+setInterval(() => {
+   createBanner();
+}, 30000)
 
 // 分享
 var height2 = windowHeight * 0.153;
